0e5972cf8e4ee44ddc652ad9a70011e735f531e5
Nate Kerksick <nate@culturegraphic.com>
2018-09-16 15:47:23 -0700

    Removes big logging

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var workExperience = require('../data/work-experience.js');
-  console.log(workExperience);
   res.render('index', {
     title: 'Nate Kerksick',
     workExperience
